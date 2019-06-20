48e5b4ab51e223d0135022ab7ba0429cc8310d2b
Alicia Wilkerson <matalina@gmail.com>
2019-06-20 06:50:18 -0500

    start of basic theme

---
+++ .vuepress/theme/layouts/Character.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Content/>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style lang="styl">
+
+<style>
