f294bb068489ab70a6d441f840d69f9437a1c7e0
Alicia Wilkerson <matalina@gmail.com>
2019-06-20 18:43:35 -0500

    basic template

---
+++ .vuepress/theme/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <component :is="layout"></component>
+  </div>
+</template>
+
+<script>
+import  Home from './layouts/Home';
+
+export default {
+  components: { Home },
+  computed: {
+      layout() {
+        return this.$page.frontmatter.layout || 'Home'
+      }
+    },
+}
+</script>
---
+++ .vuepress/theme/layouts/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <Content/>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+
+    };
+  }
+}
+</script>
